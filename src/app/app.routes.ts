4b309c018044586356090825cd8e4491f906d3c1
mareme <mariamathiaw92@gmail.com>
2024-07-30 13:09:18 +0000

    Configuration des routes pour naviguer entre les composants

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,15 @@
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { Routes } from '@angular/router';
+import { ArticleListComponent } from './components/article-list/article-list.component';
+import { ArticleDetailComponent } from './components/article-detail/article-detail.component';
+import { ArticleFormComponent } from './components/article-form/article-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: ArticleListComponent },
+  { path: 'article/:id', component: ArticleDetailComponent },
+  { path: 'add', component: ArticleFormComponent },
+  { path: 'edit/:id', component: ArticleFormComponent }
+];
 
 export const providers = [
     provideHttpClient(withInterceptors([]))
