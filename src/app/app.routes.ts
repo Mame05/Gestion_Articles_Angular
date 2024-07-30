13bdd9005e63004fd58fdbb08701dafa05bf275a
mareme <mariamathiaw92@gmail.com>
2024-07-30 12:30:20 +0000

    Installation des dépendances nécessaires pour effectuer des requêtes HTTP (@angular/common/http).J'ai apporté des modifications dans le fichier app.routes.ts et dans main.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [];
+
+export const providers = [
+    provideHttpClient(withInterceptors([]))
+  ];
