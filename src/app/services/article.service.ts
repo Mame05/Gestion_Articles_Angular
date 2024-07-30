025aab01da6070f94d5b70c2dfe37b914df2d243
mareme <mariamathiaw92@gmail.com>
2024-07-30 13:19:35 +0000

    Ajout de quelques manquements

--- src/app/services/article.service.ts
+++ src/app/services/article.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Article } from '../models/article';
 
 interface Article {
   id: number;
