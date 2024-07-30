d78ef50c12bb92e72a9724f1da6d080bfd9e04d0
mareme <mariamathiaw92@gmail.com>
2024-07-30 13:00:45 +0000

    Création des composants pour lister,créer ou modifier et supprimer un article. Création d'un service Angular et implémentation des méthodes du service pour les opérations CRUD

---
+++ src/app/services/article.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+interface Article {
+  id: number;
+  title: string;
+  body: string;
+}
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ArticleService {
+  private apiUrl = 'https://jsonplaceholder.typicode.com/posts';
+
+  constructor(private http: HttpClient) { }
+  getArticles(): Observable<Article[]> {
+    return this.http.get<Article[]>(this.apiUrl);
+  }
+
+  getArticle(id: number): Observable<Article> {
+    return this.http.get<Article>(`${this.apiUrl}/${id}`);
+  }
+
+  addArticle(article: Article): Observable<Article> {
+    return this.http.post<Article>(this.apiUrl, article);
+  }
+
+  updateArticle(article: Article): Observable<Article> {
+    return this.http.put<Article>(`${this.apiUrl}/${article.id}`, article);
+  }
+
+  deleteArticle(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+}
